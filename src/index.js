929a59270af70d28b677b57c88386dc913140976
userkamaljha <userkamaljha@gmail.com>
2024-02-03 19:32:52 +0530

    Add register controller

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ dotenv.config({path:'./env'})
 
 connectDB()
 .then(()=>{
-    app.listen(process.env.PORT) || 8000, ()=>{
-        console.log(`server is running at PORT: ${process.env.PORT}`);
-    }
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`⚙️ server is running at PORT: ${process.env.PORT}`);
+    })
 })
 .catch((err)=>{
     console.log('Mongo DB connection error', err);
