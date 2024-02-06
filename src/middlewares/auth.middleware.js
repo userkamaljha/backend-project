ec5b66165cb4bdb5743c2560882440bc7482ab4c
userkamaljha <userkamaljha@gmail.com>
2024-02-06 17:07:41 +0530

    Add refreshToken endpoint

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -7,7 +7,6 @@ import { User } from "../models/user.model.js";
 export const verifyJWT = asyncHandler(async(req, res , next)=>{
    try {
     const token =  req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
-    console.log(req.cookies);
     if(!token){
      throw new ApiError(401, "unauthorizated ruquest")
     }
@@ -26,27 +25,3 @@ export const verifyJWT = asyncHandler(async(req, res , next)=>{
      }
 })
  
-// export const verfiyJWT = asyncHandler(async(req, _ , next)=>{
-//     const token = req.cookies?.accessToken || req.header("Authorization")?.replace('Bearer ', "")
-//     console.log('TOKEN', token);
-    
-//     try {
-
-    
-//     if(!token){
-//     throw new ApiError(401, "Unauthorized Request!")
-//     }
-    
-//     let decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-    
-//     let user = await User.findById(decodedToken?._id).select('-password -refreshToken')
-    
-//     if(!user){
-//         throw new ApiError(401, 'Invalid Access Token')
-//     }
-//     req.user = user;
-//     next()
-// } catch (error) {
-//     throw new ApiError(401,error?.message || 'Invalid Access Token')
-// }
-// })
