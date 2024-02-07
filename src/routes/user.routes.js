330c74bf461516a2f9916abde755d98f3025783e
userkamaljha <userkamaljha@gmail.com>
2024-02-07 16:28:47 +0530

    fix bug in update-coverimage route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -28,7 +28,7 @@ router.route("/register").post(
     router.route("/current-user").get(verifyJWT, getCurrentUser)
     router.route("/update-account").patch(verifyJWT, updateAccountDetails)
     router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
-    router.route("/update-coverimage").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage)
+    router.route("/update-coverimage").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
     router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
     router.route("/watch-history").get(verifyJWT, getWatchHistory)
 
